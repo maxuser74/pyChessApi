8d69ce34e876bb183c4aedeca494da1cecf48090
maxuser74 <maxpasseri@libero.it>
2023-12-04 22:18:27 +0100

    Updated

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import io
 from chessdotcom import get_player_games_by_month, Client
 import chess.pgn
 import pandas as pd
-from tabulate import tabulate
 import streamlit as st
+import datetime as dt
+from dateutil.rrule import rrule, MONTHLY
 
-period = [2023,4,12]
-player="macspacs"
+period = [2023, 4, 12]
+player = "macspacs"
 openingsurl = "https://www.chess.com/openings/"
 
 game_headers = ['Event', 'Site', 'Date', 'Round', 'White',
@@ -17,16 +18,24 @@ game_headers = ['Event', 'Site', 'Date', 'Round', 'White',
 
 df = pd.DataFrame(columns=game_headers)
 
-def parseChessdotcom():
+def parseChessdotcom(start_date, end_date):
+    # Generating a monthly date range
+    date_range = rrule(MONTHLY, dtstart=start_date, until=end_date)
+
+    # Iterating over the date range
+    parse_date_range = []
+    for d in date_range:
+        parse_date_range.append([d.strftime('%Y'), d.strftime('%m')])
+
     Client.request_config["headers"]["User-Agent"] = (
         "My Python Application. "
         "Contact me at email@example.com"
     )
 
     # Month iteration
-    for month in range(period[1],period[2]+1):
-        games = get_player_games_by_month(player,period[0],month).json['games']
-        for i in range(0,(len(games)-1)):
+    for year, month in parse_date_range:
+        games = get_player_games_by_month(player, year, month).json['games']
+        for i in range(0, (len(games) - 1)):
             row = []
             pgn = io.StringIO(games[i]['pgn'])
             curr_game = chess.pgn.read_game(pgn)
@@ -37,12 +46,12 @@ def parseChessdotcom():
                     row.append("ND")
             df.loc[len(df)] = row
 
-def createStatDF():
 
+def createStatDF():
     # Define color played by user
     df["User played"] = ""
 
-    # Excplicit user ELO
+    # Explicit user ELO
     df["User ELO"] = ""
 
     # User result
@@ -52,33 +61,32 @@ def createStatDF():
 
     for i in range(len(df)):
         # White
-        if(df.loc[i, "White"]==player):
+        if df.loc[i, "White"] == player:
             df.loc[i, "User played"] = "White"
             df.loc[i, "User ELO"] = df.loc[i, "WhiteElo"]
 
-            if(df.loc[i, "Result"]=="1-0"):
+            if df.loc[i, "Result"] == "1-0":
                 df.loc[i, "User result"] = "Won"
-            if (df.loc[i, "Result"] == "0-1"):
+            if df.loc[i, "Result"] == "0-1":
                 df.loc[i, "User result"] = "Lost"
-            if (df.loc[i, "Result"] == "1/2-1/2"):
+            if df.loc[i, "Result"] == "1/2-1/2":
                 df.loc[i, "User result"] = "Draw"
 
         # Black
-        if(df.loc[i, "Black"]==player):
+        if df.loc[i, "Black"] == player:
             df.loc[i, "User played"] = "Black"
             df.loc[i, "User ELO"] = df.loc[i, "BlackElo"]
 
-            if (df.loc[i, "Result"] == "0-1"):
+            if df.loc[i, "Result"] == "0-1":
                 df.loc[i, "User result"] = "Won"
-            if (df.loc[i, "Result"] == "1-0"):
+            if df.loc[i, "Result"] == "1-0":
                 df.loc[i, "User result"] = "Lost"
-            if (df.loc[i, "Result"] == "1/2-1/2"):
+            if df.loc[i, "Result"] == "1/2-1/2":
                 df.loc[i, "User result"] = "Draw"
 
         if openingsurl in df.loc[i, "ECOUrl"]:
-            df.loc[i, "Chessdotcom Opening Desc"] = df.loc[i, "ECOUrl"].replace(openingsurl,"")
+            df.loc[i, "Chessdotcom Opening Desc"] = df.loc[i, "ECOUrl"].replace(openingsurl, "")
             df.loc[i, "Chessdotcom Opening Desc"] = df.loc[i, "Chessdotcom Opening Desc"].replace("-", " ")
-
 
     df[['User ELO']] = df[['User ELO']].apply(pd.to_numeric)
 
@@ -86,21 +94,38 @@ def writeXSLX():
     df.to_excel('output.xlsx', index=False)
 
 def main():
-    df = None
-
-    #parseChessdotcom()
-    #createStatDF()
-    #writeXSLX()
+    # createStatDF()
+    # writeXSLX()
 
     with st.sidebar:
-        st.write("This code will be printed to the sidebar.")
+        st.image('chessdotcomlogo.png', caption='Chess.com')
+
+        start_date = st.date_input(
+            "From",
+            dt.date.today() - dt.timedelta(days=31),
+            key="key_from",
+            format="DD.MM.YYYY"
+        )
+
+        end_date = st.date_input(
+            "To",
+            "today",
+            key="key_to",
+            format="DD.MM.YYYY"
+        )
+
+        if st.button("Parse Chess.com"):
+            parseChessdotcom(start_date, end_date)
+            createStatDF()
+
         uploaded_file = st.file_uploader("Choose a file")
         if uploaded_file is not None:
+            global df
             df = pd.read_excel(uploaded_file, index_col=None)
+            createStatDF()
 
-    if df is not None:
-        st.write("### Chess stats", df.head().sort_index())
-        st.write("### Chess stats", df.tail().sort_index())
+    st.write("### Chess stats")
+    st.dataframe(df)
 
 
 if __name__ == "__main__":
